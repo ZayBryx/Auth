0da020f06a9a84e4a6fa3e12497e926802fd3307
ZyBryx <zybryxm@gmail.com>
2024-04-12 15:15:15 +0800

    Admin functionalities

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,27 @@
+const { StatusCodes } = require("http-status-codes");
+const Account = require("../models/Account");
+const Token = require("../models/Token");
+
 const admin = async (req, res) => {
-  res.send("Admin Home");
+  const account = await Account.find({ role: "user" }).select(
+    "name email role"
+  );
+
+  res.status(StatusCodes.OK).json(account);
 };
 
 const logout = async (req, res) => {
-  res.send("Admin Logout");
+  const { jwt } = req.cookies;
+  const { userId } = req.user;
+
+  if (!jwt) throw new NotFoundError("Invalid Token");
+
+  const token = await Token.findOne({ refreshToken: jwt, userId });
+
+  token.isValid = false;
+
+  res.clearCookie("jwt");
+  res.status(StatusCodes.OK).json({ success: true });
 };
 
 module.exports = { admin, logout };
