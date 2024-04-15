cce854b2464ad7d3adc0fa15a7656025a056871f
ZyBryx <zybryxm@gmail.com>
2024-04-15 23:12:31 +0800

    Modify url to CLIENT URL

--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -8,7 +8,7 @@ const sendVerificationEmail = async ({ to, verificationToken, userId }) => {
     },
     to: to,
     subject: "Verify Gmail Account",
-    html: `<p>Please click the following link to verify your account: <a href="http://localhost:2024/api/auth/verify?token=${verificationToken}&id=${userId}">Verify</a></p>`,
+    html: `<p>Please click the following link to verify your account: <a href="${process.env.CLIENT_URL}auth/verify?token=${verificationToken}&id=${userId}">Verify</a></p>`,
   };
 
   await sendEmail(mailOptions);
