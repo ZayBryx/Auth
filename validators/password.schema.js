02037737a95bf8905e6894a2371da0379d9e51fc
ZyBryx <zybryxm@gmail.com>
2024-04-19 20:47:13 +0800

    ADD: password schema and MODIFY: file name

--- validators/password.schema.js
+++ validators/password.schema.js
@@ -2,20 +2,20 @@ const Joi = require("joi");
 
 const changePasswordSchema = Joi.object({
   currentPassword: Joi.string().required().min(6).messages({
-    "string.empty": "Password is required",
-    "any.required": "Password is required",
+    "string.empty": "Current Password is required",
+    "any.required": "Current Password is required",
     "string.min": "Password must be more than 6 characters",
   }),
   newPassword: Joi.string().required().min(6).messages({
-    "string.empty": "Password is required",
-    "any.required": "Password is required",
+    "string.empty": "New Password is required",
+    "any.required": "New Password is required",
     "string.min": "Password must be more than 6 characters",
   }),
   confirmPassword: Joi.string()
     .required()
     .valid(Joi.ref("newPassword"))
     .messages({
-      "any.requred": "Please confirm your password",
+      "any.required": "Please confirm your password",
       "any.only": "Password do not match",
     }),
 });
