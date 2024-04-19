abf4c19321a361591900743ed54ea83de51b5c0d
ZyBryx <zybryxm@gmail.com>
2024-04-20 00:12:57 +0800

    abortEarly to false

--- middleware/Validator.js
+++ middleware/Validator.js
@@ -3,7 +3,7 @@ const { BadRequestError } = require("../Errors");
 
 const Validator = (schema) => {
   return (req, res, next) => {
-    const { error, value } = schema.validate(req.body, { abortEarly: true });
+    const { error, value } = schema.validate(req.body, { abortEarly: false });
     if (error) throw new BadRequestError(error.message);
     req.body = value;
     next();
