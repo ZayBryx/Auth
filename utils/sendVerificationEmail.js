62b9d876af92517a4f47be8d4f62341bd5a16cdf
ZyBryx <zybryxm@gmail.com>
2024-11-24 00:41:23 +0800

    ✅ change url

--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -8,7 +8,7 @@ const sendVerificationEmail = async ({ to, verificationToken, userId }) => {
     },
     to: to,
     subject: "Verify Gmail Account",
-    html: `<p>Please click the following link to verify your account: <a href="${process.env.CLIENT_URL}auth/verify?token=${verificationToken}&id=${userId}">Verify</a></p>`,
+    html: `<p>Please click the following link to verify your account: <a href="${process.env.CLIENT_URL}api/verify?token=${verificationToken}&id=${userId}">Verify</a></p>`,
   };
 
   await sendEmail(mailOptions);
